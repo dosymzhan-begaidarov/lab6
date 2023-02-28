eb8f6e3b19312dcf4b2b3750b615cf419ea4cddf
dosymzhan-begaidarov <84137312+dosymzhan-begaidarov@users.noreply.github.com>
2023-02-28 22:12:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// Exercise 8 (Transfer USD to KZT)
+let usd = 100;
+let kzt = usd * 445;
+
+console.log(kzt)
+
+// Exercise 11 Even or Off
+
+let a = 17;
+if (a % 2 == 0){
+    console.log("The number is Even")
+}
+else{
+    console.log("The number is Odd")
+}
+
+// Exercise 14 Greatest among three
+
+let x = 10;
+let y = 15;
+let z = 5;
+
+if (x > y && x > z){
+    console.log(x)
+}
+if (y > x && y > z){
+    console.log(y)
+}
+if (z > x && z > y){
+    console.log(z)
+}
+
+
